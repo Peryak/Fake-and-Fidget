52e2847ea5b50396984841d8423088fd9dd0dca0
Peryak <coutur_h@etna-alternance.net>
2016-05-04 11:13:11 +0200

    first commit dossier propre

---
+++ F&F.js
@@ -0,0 +1,34 @@
+function menu(id) {
+    switch (id) {
+        case 1:
+            alert("1");
+            break;
+        case 2:
+            alert("2");
+            break;
+        case 3:
+            alert("3");
+            break;
+        case 4:
+            alert("4");
+            break;
+        case 5:
+            alert("5");
+            break;
+        case 6:
+            alert("6");
+            break;
+        case 7:
+            alert("7");
+            break;
+        case 8:
+            alert("8");
+            break;
+        case 9:
+            alert("9");
+            break;
+        default:
+            alert("menu");
+            break;
+    }
+}
